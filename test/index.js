7c2d48a5054211654d9ef49eaae6dd836b252d33
Paul Anderson <paul.anderson@uberops.com>
2017-04-28 01:22:41 -0400

    check for 'string' not String

--- test/index.js
+++ test/index.js
@@ -5,6 +5,6 @@ tap.test('Should be able to get an access token with valid credentials', (t) => 
   return auth({ username: 'test', password: 'test' })
   .then((token) => {
     t.ok(token)
-    t.type(token, String)
+    t.type(token, 'string')
   })
 })
