ba11ca1bee2880e6dc17cb7e5170def9fa0e7c86
Paul Anderson <paul.anderson@uberops.com>
2017-04-28 01:01:23 -0400

    setting up the basics

---
+++ index.js
@@ -0,0 +1,21 @@
+const superagent = require('superagent')
+
+module.exports = ({
+  host = 'http://localhost:8080',
+  realm = 'master',
+  client = 'admin-cli',
+  grantType = 'password',
+  clientSecret,
+  username,
+  password
+} = {}) => superagent
+.post(`${host}/auth/realms/${realm}/protocol/openid-connect/token`)
+.type('form')
+.send({
+  client_id: client,
+  grant_type: grantType,
+  client_secret: clientSecret,
+  username,
+  password
+})
+.then(auth => auth.access_token)
---
+++ test/index.js
@@ -0,0 +1,10 @@
+const tap = require('tap')
+const auth = require('../')
+
+tap.test('Should be able to get an access token with valid credentials', (t) => {
+  return auth({ username: 'test', password: 'test' })
+  .then((token) => {
+    t.ok(token)
+    t.type(token, String)
+  })
+})
